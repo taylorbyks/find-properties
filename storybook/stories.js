7e5ce230aca4ce96400b00e2d2c13f765587660f
Taylor Schinvelski <57015327+taylorbyks@users.noreply.github.com>
2021-07-15 10:46:56 -0300

    Delete storybook directory

--- storybook/stories.js
+++
@@ -1,5 +0,0 @@
-import '../src/components/atoms/Text/index.stories'
-import '../src/components/atoms/Button/index.stories'
-import '../src/components/atoms/IconButton/index.stories'
-import '../src/components/molecules/Input/index.stories'
-import '../src/components/molecules/HouseCard/index.stories'

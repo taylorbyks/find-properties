8cb87da5728768dcec0c0dc99d719b11a8fbaf55
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-21 21:48:00 -0300

    Add onPress

--- src/components/atoms/IconButton/index.js
+++ src/components/atoms/IconButton/index.js
@@ -3,9 +3,9 @@ import { IconButtonContainer } from './styles'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
-export const IconButton = ({ iconName, transparent = false }) => {
+export const IconButton = ({ iconName, transparent = false, onPress }) => {
   return (
-    <IconButtonContainer transparent={transparent}>
+    <IconButtonContainer onPress={onPress} transparent={transparent}>
       <Icon name={iconName} color="white" size={20} />
     </IconButtonContainer>
   )

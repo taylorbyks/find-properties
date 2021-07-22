4f94716ee69b512e4ec96490004a3e52250ae287
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-21 21:48:41 -0300

    Fix style of loading container

--- src/components/atoms/Loading/styles.js
+++ src/components/atoms/Loading/styles.js
@@ -6,6 +6,7 @@ export const LoadingContainer = styled.View`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    width: 100%;
     padding: ${theme.metrics.px(24)}px;
   `}
 `

f4ce0910adaa41da5ecaa0d6ebe88d9f7168b3fc
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-24 14:24:31 -0300

    Add fill option

--- src/components/atoms/IconButton/index.js
+++ src/components/atoms/IconButton/index.js
@@ -3,10 +3,10 @@ import { IconButtonContainer } from './styles'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
-export const IconButton = ({ iconName, transparent = false, onPress }) => {
+export const IconButton = ({ iconName, transparent = false, onPress, fill }) => {
   return (
     <IconButtonContainer onPress={onPress} transparent={transparent}>
-      <Icon name={iconName} color="white" size={20} />
+      <Icon name={iconName} color={fill ? 'yellow' : 'white'} size={20} />
     </IconButtonContainer>
   )
 }

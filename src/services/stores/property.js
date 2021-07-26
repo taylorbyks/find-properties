75f02cc6475bf7a0f96c864d5f7ec33ab5fada71
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 00:59:20 -0300

    Add new global state

--- src/services/stores/property.js
+++ src/services/stores/property.js
@@ -1,6 +1,15 @@
 import create from 'zustand'
 
 export const usePropertiesStore = create(set => ({
+  offset: 0,
+  setOffset: offset => set({ offset }),
+
+  query: null,
+  setQuery: query => set({ query }),
+
+  loadingProperties: true,
+  setLoadingProperties: loadingProperties => set({ loadingProperties }),
+
   properties: [],
   setProperties: properties => set({ properties }),
 

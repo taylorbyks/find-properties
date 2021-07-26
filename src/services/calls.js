8362f67b513b173fdcb48f558c28d3c9800dca4e
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 00:59:00 -0300

    Add query and offset

--- src/services/calls.js
+++ src/services/calls.js
@@ -1,15 +1,42 @@
 import api from './api'
 
-export async function getPropertiesCall() {
+export async function getPropertiesCall(query) {
+  const { sizeMin, sizeMax, priceMin, priceMax, bedsMin, bathsMin, offset } = query
+  var params = {
+    city: 'Miami',
+    state_code: 'FL',
+    limit: 15,
+    offset,
+    sort: 'relevance',
+  }
+
+  if (sizeMin) {
+    params['sqft_min'] = sizeMin
+  }
+
+  if (sizeMax) {
+    params['sqft_max'] = sizeMax
+  }
+
+  if (priceMin) {
+    params['price_min'] = priceMin
+  }
+
+  if (priceMax) {
+    params['price_max'] = priceMax
+  }
+
+  if (bathsMin) {
+    params['beds_min'] = bathsMin
+  }
+
+  if (bedsMin) {
+    params['baths_min'] = bedsMin
+  }
+
   try {
     const result = await api.get('properties/v2/list-for-rent', {
-      params: {
-        city: 'Miami',
-        state_code: 'FL',
-        limit: 15,
-        offset: 0,
-        sort: 'relevance',
-      },
+      params,
     })
 
     return result.data
